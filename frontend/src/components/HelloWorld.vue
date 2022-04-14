8eaa562183e4d0cc94edc1d41bb039e4dd4d215d
Jeonghwa <jeonghwa.jeong@kt.com>
2022-04-14 12:56:31 +0900

    #3 사용자 입력으로 별 그리기

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,57 +1,60 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>First Pattern</h2>
+<!--    <h2>First Pattern</h2>-->
+<!--    <div id="pattern">-->
+<!--      <ul v-for="n in 5">-->
+<!--        <span v-for="m in n">*</span>-->
+<!--      </ul>-->
+<!--    </div>-->
+    <div id="stars">
+      <h3>How many lines? (1...100)</h3>
+      <p></p>
+      <input v-model.number="count">
+      <p>{{count}}</p>
+    </div>
     <div id="pattern">
-      <ul v-for="n in 5">
+      <ul v-for="n in getNum">
         <span v-for="m in n">*</span>
       </ul>
     </div>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Component from 'vue-class-component';
+// import { Watch } from 'vue-property-decorator';
 
 @Options({
   props: {
     msg: String
   }
 })
+
 export default class HelloWorld extends Vue {
-  msg: string='hello world';
+  // msg: string='hello world';
+  msg: string='STARS';
+  count = 0;
+
+  onChange(num:number){
+    if(num >= 0 && num<=100) {
+      this.count = num;
+    } else {
+      alert('Please enter only number between 1 and 100')
+    }
+  }
+  get getNum(){
+    if(this.count >= 0 && this.count<=100) {
+      return this.count;
+    } else {
+      alert('Please enter only number between 1 and 100')
+      this.count = 0;
+    }
+    return this.count
+  }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -72,7 +75,7 @@ a {
 }
 #pattern{
   margin: auto;
-  width:100px;
+  width:500px;
   text-align: left;
   //background: bisque;
 }
