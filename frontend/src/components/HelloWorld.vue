160f6a5d4874b2042628c394fe5df3e37e2a83e3
Jeonghwa <jeonghwa.jeong@kt.com>
2022-05-03 20:14:19 +0900

    #4 패턴 추가

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,22 +2,22 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <div id="stars">
+      <h2>Select the Pattern</h2>
+      <select name="ptn_list" v-model="ptn" @change="changePattern">
+        <option value="ptnA">직각삼각형 - 왼쪽 아래로</option>
+        <option value="ptnB">직각삼각형 - 오른쪽 위로</option>
+        <option value="ptnC">다이아몬드</option>
+      </select>
       <h3>How many lines? (1...100)</h3>
-      <p></p>
-      <input type="number" v-model="count">
-      <p>{{count}}</p>
+      <input type="number" v-model="count" @change="getNum">
     </div>
-    <div id="pattern">
-      <ul v-for="n in getNum()">
-        <span v-for="m in n">*</span>
-      </ul>
-    </div>
+    <br>
+    <div id="pattern"></div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import Component from 'vue-class-component';
 
 @Options({
   props: {
@@ -26,20 +26,72 @@ import Component from 'vue-class-component';
 })
 
 export default class HelloWorld extends Vue {
-  msg: string = 'STARS';
-  count: number|null = null;
+  private msg: string = 'STARS';
+  private count?: number = 0;
+  private ptn: string = 'ptnA';
+
+  private changePattern(event:Event){
+    this.getNum();
+    this.getPattern();
+  }
 
   private getNum(){
-    console.log('count = ', this.count);
-    if(this.count != null) {
+    if(this.count != undefined) {
       if (this.count >= 0 && this.count <= 100) {
-        return this.count;
+        if(this.ptn == 'ptnC'){
+          if(this.count%2 == 0){
+            this.count = undefined;
+            document.getElementById("pattern").innerHTML='';
+            alert('This is Diamond Pattern.\nPlease enter only ODD number between 1 and 99')
+          }
+        }
+        this.getPattern();
       } else {
         alert('Please enter only number between 1 and 100')
-        this.count = null;
+        this.count = undefined;
       }
     }
-    return this.count
+  }
+  private getPattern(){
+    if(this.count != undefined) {
+      let cnt:number = this.count;
+      let pattern:string = '';
+      let alignStyle:string = 'left';
+
+      if (this.ptn == 'ptnA') {
+        for (let i=0; i<cnt; i++) {
+          for (let j=0; j<=i; j++) {
+            pattern += '*';
+          }
+          pattern += '<br>';
+        }
+      } else if (this.ptn == 'ptnB') {
+        alignStyle='right';
+        for (let i=0; i<cnt; i++) {
+          for (let j=0; j<cnt-i; j++) {
+            pattern += '*';
+          }
+          pattern += '<br>';
+        }
+      } else if (this.ptn == 'ptnC') {
+        alignStyle='center';
+        let quo:number = Math.floor(cnt/2);
+        for (let i=0; i<quo; i++) {
+          for (let j=0; j<2*i+1; j++) {
+            pattern += '*';
+          }
+          pattern += '<br>';
+        }
+        for (let i=quo; i>=0; i--) {
+          for (let j=0; j<2*i+1; j++) {
+            pattern += '*';
+          }
+          pattern += '<br>';
+        }
+      }
+      document.getElementById("pattern").style.textAlign=alignStyle;
+      document.getElementById("pattern").innerHTML=pattern;
+    }
   }
 }
 
